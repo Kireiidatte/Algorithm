1271cd0a1e2666018d9934a90130a3d557cc777a
TODSO <50938440+TODSO@users.noreply.github.com>
2021-04-11 16:51:49 +0900

    Create N으로 표현.py
    프로그래머스 : N으로 표현

---
+++ Programmers/N으로 표현.py
@@ -0,0 +1,26 @@
+def solution(N, number):
+    answer = -1
+    dp = []
+    
+    for i in range(1, 9):
+        numbers = set()
+        numbers.add(int(str(N) * i))
+    
+        for j in range(i - 1):
+            for x in dp[j]:
+                for y in dp[-j-1]:
+                    numbers.add(x + y)
+                    numbers.add(x - y)
+                    numbers.add(x * y)
+                    if y != 0:
+                        numbers.add(x // y)
+        
+        if number in numbers:
+            answer = i
+            break
+        print(numbers)
+        dp.append(numbers)
+        
+    return answer
+
+print(solution(5, 12))
