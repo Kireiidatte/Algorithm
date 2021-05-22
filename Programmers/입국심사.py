1e858c7e264defa400de285b0d2c3d9370e6970b
TODSO <50938440+TODSO@users.noreply.github.com>
2021-05-22 16:12:44 +0900

    Create 입국심사.py
    Programmers - 입국심사

---
+++ Programmers/입국심사.py
@@ -0,0 +1,24 @@
+def solution(n, times):
+    answer = 0
+    times.sort()
+     
+    left = 1
+    right = times[-1] * n
+     
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+        
+        for time in times:
+            cnt += mid // time
+             
+            if cnt >= n: break
+
+        if cnt >= n:
+            answer = mid
+            right = mid - 1
+        
+        else:
+            left = mid + 1
+         
+    return answer
