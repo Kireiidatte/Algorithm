db938c88324191eea855d6af50cd2fdc8079e559
TODSO <50938440+TODSO@users.noreply.github.com>
2021-10-12 13:03:33 +0900

    BOJ - DP

---
+++ Baekjoon Online Judge/2 X n 타일링 2.py
@@ -0,0 +1,12 @@
+def go(n):
+    d = [0] * (1001)
+    d[0] = 1
+    d[1] = 1
+    
+    for i in range(2, n + 1):
+        d[i] = d[i - 1] + 2 * d[i - 2]
+    
+    return d[n] % 10007
+    
+n = int(input())
+print(go(n))
