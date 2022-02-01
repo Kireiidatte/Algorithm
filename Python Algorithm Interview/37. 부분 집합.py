79075068eed210a87ec2054de6519a1ec0d7d2b4
TODSO <50938440+TODSO@users.noreply.github.com>
2022-02-01 16:18:09 +0900

    Create 37. 부분 집합.py

---
+++ Python Algorithm Interview/37. 부분 집합.py
@@ -0,0 +1,12 @@
+def subsets(nums):
+    result = []
+
+    def dfs(idx, path):
+        result.append(path)
+
+        for i in range(idx, len(nums)):
+            dfs(i+1, path + [nums[i]])
+    
+    dfs(0, [])
+    
+    return result
