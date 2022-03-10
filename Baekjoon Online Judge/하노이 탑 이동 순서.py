950e942ca17a6684142b82dffa5e54ba88d9c1ea
TODSO <50938440+TODSO@users.noreply.github.com>
2022-03-10 13:46:28 +0900

    Update 하노이 탑 이동 순서.py

--- Baekjoon Online Judge/하노이 탑 이동 순서.py
+++ Baekjoon Online Judge/하노이 탑 이동 순서.py
@@ -1,22 +1,12 @@
 N = int(input())
-cnt = 0
 
 def solve(n, start, to):
-    global cnt
     if n == 0:
         return
-    cnt += 1
     solve(n-1, start, 6-start-to)
-    solve(n-1, 6-start-to, to)
-
-def printSolve(n, start, to):
-    if n == 0:
-        return
-    printSolve(n-1, start, 6-start-to)
     print('{0} {1}'.format(start, to))
-    printSolve(n-1, 6-start-to, to)
-
+    solve(n-1, 6-start-to, to)
+    
+print((1 << N) - 1)
 solve(N, 1, 3)
-print(cnt)
-printSolve(N, 1, 3)
 
