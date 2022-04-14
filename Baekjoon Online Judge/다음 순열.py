b0203b6b2009320a3df8c972103bfbecb57d1271
TODSO <50938440+TODSO@users.noreply.github.com>
2022-04-14 13:31:39 +0900

    BOJ - 다음 순열

---
+++ Baekjoon Online Judge/다음 순열.py
@@ -0,0 +1,24 @@
+def next_permutaion(list):
+    i = len(list)-1
+    j = len(list)-1
+    while i>0 and list[i-1] >= list[i]:
+        i -= 1
+    if i<= 0:
+        return False
+    while list[j] <= list[i-1]:
+        j -= 1 
+    
+    list[i-1], list[j] = list[j], list[i-1]
+    j = len(list)-1
+    while i < j:
+        list[i],list[j] = list[j],list[i]
+        i += 1
+        j -= 1
+    return True
+N = int(input())
+arr = list(map(int,input().split()))
+
+if next_permutaion(arr):
+    print(' '.join(map(str, arr)))
+else:
+    print(-1)
