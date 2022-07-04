a6d6d1a30b228da8b4831c8ae1c2d7b4cd37ac65
TODSO <50938440+TODSO@users.noreply.github.com>
2022-07-04 09:02:35 +0900

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,6 +1,7 @@
 array = [8, 4, 6, 2, 9, 1, 3, 7, 5]
 
 def insertionSort(array):
+    # 첫 번째 값은 정렬이 됐다고 가정
     for i in range(1, len(array)):
         # 뒤에서 하나씩 비교
         for j in range(i, 0, -1):
