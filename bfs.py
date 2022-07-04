e690cb5c8818fdc3e882d607c83310c653df9bf1
TODSO <50938440+TODSO@users.noreply.github.com>
2022-07-04 09:02:30 +0900

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -6,14 +6,14 @@ matrix = [[0]*(N + 1) for i in range(N + 1)]
 for i in range(M):
     a, b = map(int, input().split())
     matrix[a][b] = matrix[b][a] = 1
-visit_list = [0]*(N + 1)
+visit_list = [False]*(N + 1)
 
 def BFS(V):
     dq = deque([V])
-    visit_list[V] = 0
+    visit_list[V] = True
     while dq:
         V = dq.popleft()
         for i in range(1, N + 1):
-            if visit_list[i] == 1 and matrix[V][i] == 1:
+            if not visit_list[i] and matrix[V][i] == 1:
                 dq.append(i)
-                visit_list[i] = 0
+                visit_list[i] = True
